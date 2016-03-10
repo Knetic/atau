3e6dc9e7e1e5fab2e082b79f1ec5119caa37b13a
= <glester491@gmail.com>
2016-03-10 00:55:30 -0800

    Added snippet for rb

---
+++ src/atau/codegen_rb.go
@@ -0,0 +1,13 @@
+package atau
+
+/*
+require 'net/http'
+require 'uri'
+
+uri = URI.parse(url)
+
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Put.new(uri.path)
+request.body = "{}"
+http.request(request)
+*/
